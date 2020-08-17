155d0ffa164b49f2b6a39b52bb6c14af3381250b
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-08-17 15:47:59 +0200

    Add tests for chained combinations

--- test/rmby.test.ts
+++ test/rmby.test.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { readdir, stat, unlink } from "../src/asyncFs";
 import { readdir, stat, unlink } from "../src/asyncFs";
 import { RemoveFiles } from "../src/rmby";
 import fs from "fs";
@@ -20,51 +19,56 @@ const dirContent = [
   "dir2",
   "file3.html",
   "file4.js",
+  "file5.js",
 ];
 const file1 = "/path/to/dir/file1.txt";
 const file2 = "/path/to/dir/file2.css";
 const file3 = "/path/to/dir/file3.html";
 const file4 = "/path/to/dir/file4.js";
+const file5 = "/path/to/dir/file5.js";
 const dir1 = "/path/to/dir/dir1";
 const dir2 = "/path/to/dir/dir2";
 
 describe("rmby", () => {
+  beforeEach(() => {
+    const stats: { [key: string]: any } = {
+      "/path/to/dir/file1.txt": {
+        isFile: () => true,
+        mtime: new Date(Date.now() - 500),
+      },
+      "/path/to/dir/file2.css": {
+        isFile: () => true,
+        mtime: new Date(Date.now() - 1000 * 30),
+      },
+      "/path/to/dir/dir1": {
+        isFile: () => false,
+      },
+      "/path/to/dir/dir2": {
+        isFile: () => false,
+      },
+      "/path/to/dir/file3.html": {
+        isFile: () => true,
+        mtime: new Date(Date.now() - 1000 * 60 * 45),
+      },
+      "/path/to/dir/file4.js": {
+        isFile: () => true,
+        mtime: new Date(Date.now() - 1000 * 60 * 60 * 6),
+      },
+      "/path/to/dir/file5.js": {
+        isFile: () => true,
+        mtime: new Date(Date.now() - 1000 * 60 * 60 * 12),
+      },
+    };
+    (readdir as any).mockResolvedValue(dirContent);
+    (stat as any).mockImplementation((fileName: string) =>
+      Promise.resolve(stats[fileName]),
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
   describe("remove by time", () => {
-    beforeEach(() => {
-      const stats: { [key: string]: any } = {
-        "/path/to/dir/file1.txt": {
-          isFile: () => true,
-          mtime: new Date(Date.now() - 500),
-        },
-        "/path/to/dir/file2.css": {
-          isFile: () => true,
-          mtime: new Date(Date.now() - 1000 * 30),
-        },
-        "/path/to/dir/dir1": {
-          isFile: () => false,
-        },
-        "/path/to/dir/dir2": {
-          isFile: () => false,
-        },
-        "/path/to/dir/file3.html": {
-          isFile: () => true,
-          mtime: new Date(Date.now() - 1000 * 60 * 45),
-        },
-        "/path/to/dir/file4.js": {
-          isFile: () => true,
-          mtime: new Date(Date.now() - 1000 * 60 * 60 * 6),
-        },
-      };
-      (readdir as any).mockResolvedValue(dirContent);
-      (stat as any).mockImplementation((fileName: string) =>
-        Promise.resolve(stats[fileName]),
-      );
-    });
-
-    afterEach(() => {
-      jest.resetAllMocks();
-    });
-
     it("should remove all files older than given milliseconds", async () => {
       // arrange
       const remove = new RemoveFiles();
@@ -78,13 +82,15 @@ describe("rmby", () => {
         .run();
 
       // assert
-      expect(unlink).toHaveBeenCalledWith(file1);
-      expect(unlink).toHaveBeenCalledWith(file2);
+      expect(unlink).toHaveBeenCalledTimes(5);
       expect(unlink).not.toHaveBeenCalledWith(dir1);
       expect(unlink).not.toHaveBeenCalledWith(dir2);
+      expect(unlink).toHaveBeenCalledWith(file1);
+      expect(unlink).toHaveBeenCalledWith(file2);
       expect(unlink).toHaveBeenCalledWith(file3);
       expect(unlink).toHaveBeenCalledWith(file4);
-      expect(deletedFiles).toEqual([file1, file2, file3, file4]);
+      expect(unlink).toHaveBeenCalledWith(file5);
+      expect(deletedFiles).toEqual([file1, file2, file3, file4, file5]);
     });
 
     it("should remove all files older than given seconds", async () => {
@@ -100,13 +106,15 @@ describe("rmby", () => {
         .run();
 
       // assert
-      expect(unlink).not.toHaveBeenCalledWith(file1);
-      expect(unlink).toHaveBeenCalledWith(file2);
+      expect(unlink).toHaveBeenCalledTimes(4);
       expect(unlink).not.toHaveBeenCalledWith(dir1);
       expect(unlink).not.toHaveBeenCalledWith(dir2);
+      expect(unlink).not.toHaveBeenCalledWith(file1);
+      expect(unlink).toHaveBeenCalledWith(file2);
       expect(unlink).toHaveBeenCalledWith(file3);
       expect(unlink).toHaveBeenCalledWith(file4);
-      expect(deletedFiles).toEqual([file2, file3, file4]);
+      expect(unlink).toHaveBeenCalledWith(file5);
+      expect(deletedFiles).toEqual([file2, file3, file4, file5]);
     });
 
     it("should remove all files older than given minutes", async () => {
@@ -122,13 +130,15 @@ describe("rmby", () => {
         .run();
 
       // assert
-      expect(unlink).not.toHaveBeenCalledWith(file1);
-      expect(unlink).not.toHaveBeenCalledWith(file2);
+      expect(unlink).toHaveBeenCalledTimes(3);
       expect(unlink).not.toHaveBeenCalledWith(dir1);
       expect(unlink).not.toHaveBeenCalledWith(dir2);
+      expect(unlink).not.toHaveBeenCalledWith(file1);
+      expect(unlink).not.toHaveBeenCalledWith(file2);
       expect(unlink).toHaveBeenCalledWith(file3);
       expect(unlink).toHaveBeenCalledWith(file4);
-      expect(deletedFiles).toEqual([file3, file4]);
+      expect(unlink).toHaveBeenCalledWith(file5);
+      expect(deletedFiles).toEqual([file3, file4, file5]);
     });
 
     it("should remove all files older than given hours", async () => {
@@ -144,13 +154,15 @@ describe("rmby", () => {
         .run();
 
       // assert
-      expect(unlink).not.toHaveBeenCalledWith(file1);
-      expect(unlink).not.toHaveBeenCalledWith(file2);
+      expect(unlink).toHaveBeenCalledTimes(2);
       expect(unlink).not.toHaveBeenCalledWith(dir1);
       expect(unlink).not.toHaveBeenCalledWith(dir2);
+      expect(unlink).not.toHaveBeenCalledWith(file1);
+      expect(unlink).not.toHaveBeenCalledWith(file2);
       expect(unlink).not.toHaveBeenCalledWith(file3);
       expect(unlink).toHaveBeenCalledWith(file4);
-      expect(deletedFiles).toEqual([file4]);
+      expect(unlink).toHaveBeenCalledWith(file5);
+      expect(deletedFiles).toEqual([file4, file5]);
     });
 
     it("should throw exception if readdir() goes wrong", async () => {
@@ -204,14 +216,6 @@ describe("rmby", () => {
   });
 
   describe("remove by name", () => {
-    beforeEach(() => {
-      (readdir as any).mockResolvedValue(dirContent);
-    });
-
-    afterEach(() => {
-      jest.resetAllMocks();
-    });
-
     describe("thatEquals()", () => {
       it("should remove all files that equal to provided name", async () => {
         // arrange
@@ -296,12 +300,13 @@ describe("rmby", () => {
 
         // assert
         expect(readdir).toHaveBeenCalledTimes(1);
-        expect(unlink).toHaveBeenCalledTimes(4);
+        expect(unlink).toHaveBeenCalledTimes(5);
         expect(unlink).toHaveBeenCalledWith(file1);
         expect(unlink).toHaveBeenCalledWith(file2);
         expect(unlink).toHaveBeenCalledWith(file3);
         expect(unlink).toHaveBeenCalledWith(file4);
-        expect(deletedFiles).toEqual([file1, file2, file3, file4]);
+        expect(unlink).toHaveBeenCalledWith(file5);
+        expect(deletedFiles).toEqual([file1, file2, file3, file4, file5]);
       });
 
       it("should return empty array and do nothing if no filename starts with provided name", async () => {
@@ -439,12 +444,13 @@ describe("rmby", () => {
 
         // assert
         expect(readdir).toHaveBeenCalledTimes(1);
-        expect(unlink).toHaveBeenCalledTimes(4);
+        expect(unlink).toHaveBeenCalledTimes(5);
         expect(unlink).toHaveBeenCalledWith(file1);
         expect(unlink).toHaveBeenCalledWith(file2);
         expect(unlink).toHaveBeenCalledWith(file3);
         expect(unlink).toHaveBeenCalledWith(file4);
-        expect(deletedFiles).toEqual([file1, file2, file3, file4]);
+        expect(unlink).toHaveBeenCalledWith(file5);
+        expect(deletedFiles).toEqual([file1, file2, file3, file4, file5]);
       });
 
       it("should return empty array and do nothing if no filename includes the provided name", async () => {
@@ -500,14 +506,6 @@ describe("rmby", () => {
   });
 
   describe("remove by file extension", () => {
-    beforeEach(() => {
-      (readdir as any).mockResolvedValue(dirContent);
-    });
-
-    afterEach(() => {
-      jest.resetAllMocks();
-    });
-
     it("should remove all files that equal to provided file extension", async () => {
       // arrange
       const remove = new RemoveFiles();
@@ -567,6 +565,32 @@ describe("rmby", () => {
       ).rejects.toBeTruthy();
       expect(readdir).toHaveBeenCalledTimes(1);
       expect(unlink).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("remove combinations", () => {
+    it("should be able to remove files with chained combinations", async () => {
+      // arrange
+      const remove = new RemoveFiles();
+
+      // act
+      const deletedFiles = await remove
+        .from(dirPath)
+        .byName()
+        .thatStartsWith("f")
+        .and()
+        .byExtension(".js")
+        .and()
+        .byTime()
+        .inHours()
+        .olderThan(12)
+        .run();
+
+      // assert
+      expect(readdir).toHaveBeenCalledTimes(1);
+      expect(unlink).toHaveBeenCalledTimes(1);
+      expect(unlink).toHaveBeenCalledWith(file5);
+      expect(deletedFiles).toEqual([file5]);
     });
   });
 });
